05d634c110b92256e62763ecadfde784ba4cb30d
Narbeh Malekian <narbehmalekian@gmail.com>
2022-08-03 03:29:40 -0700

    added check for admin privileges before running
    certain processes cannot be killed without admin privileges

--- KillAdobeCC.ps1
+++ KillAdobeCC.ps1
@@ -1,3 +1,22 @@
+# The following lines confirm that the script is running as an administrator
+# code from: https://superuser.com/a/532109
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+# End of Admin check
+
 $ProcessList = @(
     'AGMService'
     'acrotray'
